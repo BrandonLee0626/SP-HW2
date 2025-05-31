8912cbe60fa7b4f4b37f8d861d228e84779315b9
BrandonLee0626 <leeminjae0626@dgist.ac.kr>
2025-05-31 17:03:46 +0900

    add multi to runfile

--- runfile.sh
+++ runfile.sh
@@ -3,5 +3,5 @@ git pull
 make clean
 make
 
-./client& -ip "10.8.136.81" -port "5000" -username "Alice"
-./client& -ip "10.8.136.81" -port "5000" -username "Bob"
+./client -ip 10.8.136.81 -port 5000 -username Alice&
+./client -ip 10.8.136.81 -port 5000 -username Bob&
