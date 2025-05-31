5d07a24a7ac98dced68ae29d4dfcc75a63893e3c
BrandonLee0626 <leeminjae0626@dgist.ac.kr>
2025-05-31 16:09:27 +0900

    add " to runfile.sh

--- runfile.sh
+++ runfile.sh
@@ -5,5 +5,5 @@ make
 
 ./server&
 
-./client& -ip 10.8.131.119 -port 5000 -username Alice
-./client& -ip 10.8.131.119 -port 5000 -username Bob
+./client& -ip "10.8.131.119" -port "5000" -username "Alice"
+./client& -ip "10.8.131.119" -port "5000" -username "Bob"
