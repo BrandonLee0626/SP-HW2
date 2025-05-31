9ca314fec97bcd44f79a5f7392080b2e6f6f00f5
BrandonLee0626 <leeminjae0626@dgist.ac.kr>
2025-05-31 16:12:39 +0900

    delete server in runfile.sh

--- runfile.sh
+++ runfile.sh
@@ -3,7 +3,5 @@ git pull
 make clean
 make
 
-./server&
-
-./client& -ip "10.8.131.119" -port "5000" -username "Alice"
-./client& -ip "10.8.131.119" -port "5000" -username "Bob"
+./client& -ip "10.8.136.81" -port "5000" -username "Alice"
+./client& -ip "10.8.136.81" -port "5000" -username "Bob"
